4b62cca15e3e1fd5a39bb79ac4db21d059eb0874
Caiji Zhou <cjz210oliver@gmail.com>
2023-09-01 17:31:46 +0800

    异步inquire(version1.0.0)

--- dubbo-spring-boot-demo-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/RecordService.java
+++ dubbo-spring-boot-demo-provider/src/main/java/org/apache/dubbo/springboot/demo/provider/RecordService.java
@@ -2,6 +2,7 @@ package org.apache.dubbo.springboot.demo.provider;
 
 import org.apache.dubbo.springboot.demo.model.TReturn;
 import org.apache.dubbo.springboot.demo.model.dto.SaveRecordDto;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -12,5 +13,6 @@ import java.util.concurrent.CompletableFuture;
 public interface RecordService {
     void saveRecord(SaveRecordDto<TReturn> saveRecordDto);
 
+    @Async
     void saveRecordAsync(SaveRecordDto<CompletableFuture<TReturn>> saveRecordDto) throws Exception;
 }
